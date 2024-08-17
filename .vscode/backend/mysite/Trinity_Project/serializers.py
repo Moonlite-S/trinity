9d200fa186555cb4b6b53f94a99e5e1bd0820c16
Sean Louis Dela Cruz <77084281+Moonlite-S@users.noreply.github.com>
2024-08-17 15:11:56 -0500

    Delete .vscode directory

--- .vscode/backend/mysite/Trinity_Project/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Project
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Project
-        fields = ['Project_id','Name','Manager','Customer','Start_Date','End_Date']
