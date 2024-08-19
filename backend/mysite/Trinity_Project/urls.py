b6ae89c38fa4fbd5b36937f4c2e0118b5bf85d9c
joseagmz <joseagmz@hotmail.com>
2024-08-19 16:41:08 -0500

    added user,login,logout and cookies

--- backend/mysite/Trinity_Project/urls.py
+++ backend/mysite/Trinity_Project/urls.py
@@ -1,9 +1,14 @@
 from django.urls import  path
 from django.contrib import admin
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("projects/", views.project_list,name="project_list"),
     path("projects/<str:project_id>", views.project_detail),
+    path("register", views.RegisterView.as_view()),
+    path("login", views.LoginView.as_view()),
+    path("user",views.UserView.as_view()),    
+    path('logout', views.LogoutView.as_view())
 ]
