3d272fe255c32e24484f11662f0cff563fd9d602
Moonlite-Irony <iphoneagents00@gmail.com>
2024-08-29 15:58:43 -0500

    let's hardcode it for now

--- frontend/src/components/Axios.tsx
+++ frontend/src/components/Axios.tsx
@@ -3,8 +3,11 @@ import axios from "axios";
 const isDevelopment = import.meta.env.MODE === 'development'
 const baseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
 
+console.log('Environment:', import.meta.env.MODE);
+console.log('Base URL:', baseUrl);
+
 const AxiosInstance = axios.create({
-    baseURL: baseUrl,
+    baseURL: "https://django-react-test-e9ghbqcrf2djcmfu.israelcentral-01.azurewebsites.net/",
     timeout: 5000,
     withCredentials: true,
     headers: {
