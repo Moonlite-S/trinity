247da473d035b14ce90621c0607eaaa58042ca82
Moonlite-Irony <iphoneagents00@gmail.com>
2024-08-29 17:05:07 -0500

    maybe this'll work

--- frontend/src/components/Axios.tsx
+++ frontend/src/components/Axios.tsx
@@ -4,12 +4,9 @@ import config from "../config";
 const isDevelopment = import.meta.env.MODE === 'development'
 const baseUrl = isDevelopment ? config.development.apiBaseUrl : config.production.apiBaseUrl
 
-console.log('Environment:', import.meta.env.MODE);
-console.log('Base URL:', baseUrl);
-
 const AxiosInstance = axios.create({
     baseURL: baseUrl,
-    timeout: 5000,
+    timeout: 10000,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
