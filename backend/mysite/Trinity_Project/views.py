52d11d07a39b8d6a679430c77f50d5217d6db868
Moonlite-Irony <iphoneagents00@gmail.com>
2024-08-30 11:31:25 -0500

    ok

--- backend/mysite/Trinity_Project/views.py
+++ backend/mysite/Trinity_Project/views.py
@@ -56,7 +56,7 @@ class LoginView(APIView):
         
         response =Response()
         
-        response.set_cookie(key='jwt', value=token, httponly=True, samesite='Lax')
+        response.set_cookie(key='jwt', value=token, httponly=True, samesite='None', secure=True)
         response.data = {
             'jwt': token
         }
